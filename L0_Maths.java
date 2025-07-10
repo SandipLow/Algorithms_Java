3f2c4d29b0c804690601e5824db131ee43c17faa
SandipLow <sandiplow.official@gmail.com>
2025-07-10 18:29:08 +0530

    L0_maths

---
+++ L0_Maths.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class L0_Maths {
+
+    public static void getDigits(int num) {
+        while (num!=0) {
+            int digit = num%10;
+            System.out.println(digit);
+            num = num/10;
+        }
+    }
+
+    public static int gcd(int num1, int num2) {
+        while (num1!=0) {
+            num1 = num2 % (num2 = num1);
+        }
+
+        return num2;
+    }
+
+    public static List<Integer> divisors(int num) {
+        List<Integer> ans = new ArrayList<>();
+
+        for (int div=1; div<=Math.sqrt(num); div++) {
+            if (num%div!=0) continue;
+
+            ans.add(div);
+            
+            if (div!=num/div)
+                ans.add(num/div);
+        }
+
+        return ans;
+    }
+
+    public static boolean checkPrime(int num) {
+        if (num<=1) return false;
+        if (num<=3) return true;
+        if (num%2==0 || num%3==0) return false;
+
+        int i = 5;
+        while (i*i <= num) {
+            if (num%i==0 || num%(i+2)==0)
+                return false;
+
+            i+=6;
+        }
+
+        return true;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int num = sc.nextInt();
+        System.out.println(checkPrime(num));
+
+        sc.close();
+    }
+
+}
